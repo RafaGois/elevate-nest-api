9b42c31acf9bdf846692fed23a6ddb77d0c76654
RafaGois <rafaelfontanabg@gmail.com>
2026-06-15 15:02:31 -0300

    fix: implementation git-config interface in service

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfigInterface } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfigInterface {
+  constructor(private configService: ConfigService) {}
+  getAppPort(): number {
+    return Number(this.configService.get<number>('APP_PORT'));
+  }
+
+  getNodeEnv(): string {
+    return String(this.configService.get<string>('NODE_ENV'));
+  }
+
+  getDatabaseUrl(): string {
+    return String(this.configService.get<string>('DATABASE_URL'));
+  }
+
+  getJwtSecret(): string {
+    return String(this.configService.get<string>('JWT_SECRET'));
+  }
+}
