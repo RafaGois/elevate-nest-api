e20969a85f3e51adf93b73e6f5e9f6c6e3d43896
RafaGois <rafaelfontanabg@gmail.com>
2026-06-16 09:52:38 -0300

    fix: getters methods unit tests

--- src/users/domain/entities/init/user.entity.spec.ts
+++ src/users/domain/entities/init/user.entity.spec.ts
@@ -22,4 +22,47 @@ describe('UserEntity', () => {
     expect(sut.props.createdAt).toBeInstanceOf(Date);
     expect(sut.props.updatedAt).toBeInstanceOf(Date);
   });
+
+  it('getter of name field', () => {
+    sut = new UserEntity(props);
+
+    expect(sut.name).toBeDefined();
+    expect(typeof sut.name).toBe('string');
+    expect(sut.name.length).toBeGreaterThan(0);
+    expect(sut.name).toBe(props.name);
+  });
+
+  it('getter of email field', () => {
+    sut = new UserEntity(props);
+
+    expect(sut.email).toBeDefined();
+    expect(typeof sut.email).toBe('string');
+    expect(sut.email.length).toBeGreaterThan(0);
+    expect(sut.email).toBe(props.email);
+  });
+
+  it('getter of password field', () => {
+    sut = new UserEntity(props);
+
+    expect(sut.password).toBeDefined();
+    expect(typeof sut.password).toBe('string');
+    expect(sut.password.length).toBeGreaterThan(0);
+    expect(sut.password).toBe(props.password);
+  });
+
+  it('getter of createdAt field', () => {
+    sut = new UserEntity(props);
+
+    expect(sut.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeInstanceOf(Date);
+    expect(sut.createdAt).toBe(props.createdAt);
+  });
+
+  it('getter of updatedAt field', () => {
+    sut = new UserEntity(props);
+
+    expect(sut.updatedAt).toBeDefined();
+    expect(sut.updatedAt).toBeInstanceOf(Date);
+    expect(sut.updatedAt).toBe(props.updatedAt);
+  });
 });
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -12,4 +12,28 @@ export class UserEntity {
     this.props.createdAt = this.props.createdAt ?? new Date();
     this.props.updatedAt = this.props.updatedAt ?? new Date();
   }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get email(): string {
+    return this.props.email;
+  }
+
+  get password(): string {
+    return this.props.password;
+  }
+
+  get createdAt(): Date | undefined {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this.props.updatedAt;
+  }
 }
